2795dc511127a33740f39c8f89cf439513cc538e
Zubbbz <37497090+Zubbbz@users.noreply.github.com>
2022-12-14 14:32:24 -0500

    add day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, path::Path};
-const INPUTFILEPATH: &str = "/home/nathan/repos/advent-of-code-2022/day1/input.txt";
+const INPUTFILEPATH: &str =
+	"/home/nathan/repos/advent-of-code-2022/day1/input.txt";
 
 fn str_to_path(path: &str) -> &Path {
 	Path::new(path)
@@ -9,8 +10,9 @@ fn parse_input(path: &Path) -> Vec<Vec<u32>> {
 	let mut elves: Vec<Vec<u32>> = vec![vec![]];
 	let mut iter: usize = 0;
 
-	let data = fs::read_to_string(path)
-		.expect("Failed to open the input file (did you esure the path was correct?)");
+	let data = fs::read_to_string(path).expect(
+		"Failed to open the input file (did you esure the path was correct?)",
+	);
 
 	for line in data.lines() {
 		match line.parse() {
@@ -28,21 +30,17 @@ fn parse_input(path: &Path) -> Vec<Vec<u32>> {
 
 fn parse_elf_calories(elves: &[Vec<u32>]) -> Vec<u32> {
 	return elves
-	.iter()
-	.map(|inner_vec|
-	inner_vec
 		.iter()
-		.sum())
-		.collect();
+		.map(|inner_vec| inner_vec
+			.iter()
+			.sum())
+			.collect();
 }
 
 fn main() {
 	let elves = parse_input(str_to_path(INPUTFILEPATH));
-	let totals = parse_elf_calories(&elves);
-	let (max_idx, max) = totals
-		.iter()
-		.enumerate()
-		.max_by(|(_, a), (_, b)| a.cmp(b))
-		.unwrap();
-	println!("elves: {:#?}\ntotals: {:#?}\nidx: {}, max: {}", elves, totals, max_idx, max);
+	let mut totals = parse_elf_calories(&elves);
+	totals.sort_by(|a, b| a.cmp(b).reverse());
+	let top_three = totals[0] + totals[1] + totals[2];
+	println!("totals: {:#?}\nNumber 1: {:#?}\n Top3: {:#?}", totals, totals[0], top_three);
 }
